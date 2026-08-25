889981ba122374d9090fcf7e2327f71c548325a6
wilianromero268-gif <wilianromero268@gmail.com>
2026-08-25 13:11:17 -0500

    Update index.js
    Guarda r

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.json({
         status: true,
+        code: 200,
+        creator: 'SaiDev145',
         message: 'Saitama API funcionando',
-        creator: 'SaiDev145'
+        version: '1.0.0'
     })
 })
 
@@ -14,8 +18,15 @@ app.get('/saitama/api/test', (req, res) => {
     res.json({
         status: true,
         code: 200,
+        creator: 'SaiDev145',
         message: 'Test OK'
     })
 })
+
+if (require.main === module) {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('Saitama API funcionando')
+    })
+}
 
 module.exports = app
